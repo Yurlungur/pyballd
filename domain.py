35557bc584164e2daa79ff40f0c1ce805d75bac5
Jonah Miller <jonah.maxwell.miller@gmail.com>
2017-05-12 18:19:33 -0400

    update domain to work with x = r - rh or with x^2 = r^2 - r_h^2

--- domain.py
+++ domain.py
@@ -2,7 +2,7 @@
 
 """domain.py
 Author: Jonah Miller (jonah.maxwell.miller@gmail.com)
-Time-stamp: <2017-05-11 19:19:14 (jmiller)>
+Time-stamp: <2017-05-12 18:16:15 (jmiller)>
 
 A component of the pyballd library. This module defines the domain and
 coordinate system used for pyballd:
@@ -25,10 +25,17 @@ which we calculate numerically.
 from __future__ import print_function
 import numpy as np
 from orthopoly import PseudoSpectralStencil2D
+EPSILON_SML=1e-10
+EPSILON_BIG=1e10
 
+    
 class PyballdStencil(PseudoSpectralStencil2D):
     """A derivative stencil explicitly designed for axisymmetric
     domains. Automatically performs appropriate compactification.
+
+    This version uses
+    x = r - r_h,
+    thus shifting the domain of x to [0,infinity)
     """
     X_min = 0
     X_max = 1
@@ -57,14 +64,18 @@ class PyballdStencil(PseudoSpectralStencil2D):
                                             theta_min,theta_max)
         self.r = self.get_r_from_X(self.x)
         self.R = self.get_r_from_X(self.X)
-        self.dRdX = self.differentiate(self.R,1,0)
-        self.drdX = self.dRdx[:,0]
-        self.dXdR = 1./self.dRdX
-        self.dXdr = self.dXdR[:,0]
+        self.dRdX = self.get_drdX(self.X)
+        self.drdX = self.get_drdX(self.x)
+        self.dXdR = self.get_dXdr(self.X)
+        self.dXdr = self.get_dXdr(self.x)
+        self.d2XdR2 = self.get_d2Xdr2(self.X)
+        self.d2Xdr2 = self.get_d2Xdr2(self.x)
+        self.d2RdX2 = self.get_d2rdX2(self.X)
+        self.d2rdX2 = self.get_d2rdX2(self.x)
         self.theta = self.y
         self.THETA = self.Y
 
-    def differentiate_wrt_X(self,grid_func,order_X,order_theta):
+    def differentiate_wrt_R(self,grid_func,order_X,order_theta):
         "Differentiate a grid function on our compactified domain"
 
         assert type(order_X) is int
@@ -72,13 +83,19 @@ class PyballdStencil(PseudoSpectralStencil2D):
         assert order_X >= 0
         assert order_theta >= 0
 
-        if order_X > 0:
-            df = np.empty_like(grid_func)
-            for j in range(df.shape[1]):
-                df[:,j] = self.stencil_x.differentiate(grid_func[:,j],order_X)
+        if order_X > 2:
+            raise NotImplementedError("Derivatives in X higher than order 2"
+                                      +" not implemented.")
+        if order_X == 1:
+            df = self.differentiate(grid_func,1,order_theta)
             df *= self.dXdR
-            return self.differentiate(df,0,ordery)
-        
+            return df
+        if order_X == 2:
+            d2fdX2 = self.differentiate(grid_func,2,order_theta)
+            dfdX = self.differentiate(grid_func,1,order_theta)
+            d2fdR2 = (self.dXdR**2)*d2fdX2 + self.d2XdR2*dfdX
+            return d2fdR2
+        # else (if order_X == 0)
         return self.differentiate(grid_func,0,order_theta)
 
     def inner_product_on_sphere(self,gf1,gf2):
@@ -86,13 +103,33 @@ class PyballdStencil(PseudoSpectralStencil2D):
         factors = [s.get_scale_factor() for s in self.stencils]
         factor = np.prod(factors)
         # exclude last point because it diverges
-        integrand = (gf1*self.weights2D*gf2*(self.R**2)*np.sin(self.THETA))[:-1,0]
+        integrand = (gf1*self.weights2D*gf2
+                     *self.dRdX
+                     *(self.R**2)*np.sin(self.THETA))[:-1,0]
         integral_unit_cell = np.sum(integrand)
         integral_physical = integral_unit_cell*factor*(2*np.pi)
         return integral_physical
 
+    def get_drdX(self,X):
+        dXdr = self.get_dXdr(X)
+        drdX = 1./dXdr
+        return drdX
+
+    def get_dXdr(self,X):
+        dXdr = (X-1)**2
+        return dXdr
+
+    def get_d2rdX2(self,X):
+        d2Xdr2 = self.get_d2Xdr2(X)
+        d2rdX2 = 1./d2Xdr2
+        return d2rdX2
+
+    def get_d2Xdr2(self,X):
+        d2Xdr2 = 2*(X-1)**3
+        return d2Xdr2
+
     def get_x_from_r(self,r):
-        x = np.sqrt(r**2 - self.r_h**2)
+        x = r - self.r_h
         return x
 
     def get_X_from_x(self,x):
@@ -109,7 +146,7 @@ class PyballdStencil(PseudoSpectralStencil2D):
         return x
 
     def get_r_from_x(self,x):
-        r = np.sqrt(x**2 + r_h**2)
+        r = x + self.r_h
         return r
 
     def get_r_from_X(self,X):
@@ -128,3 +165,31 @@ class PyballdStencil(PseudoSpectralStencil2D):
         return self.R,self.THETA
 
     
+class PyballdStencilBH(PyballdStencil):
+    """A derivative stencil explicitly designed for axisymmetric
+    black holes. Automatically performs appropriate compactification.
+
+    This version uses
+    x = sqrt(r^2 - r_h^2),
+    thus shifting the domain of x to [0,infinity)
+    """
+
+    def get_dXdr(self,X):
+        num = ((X-1)**2)*np.sqrt(X**2+(self.r_h**2)*((X-1)**2))
+        denom = X
+        dXdr = num/denom
+        return dXdr
+
+    def get_d2Xdr2(self,X):
+        num = ((X-1)**3)*(2*(X**3)+(self.r_h**2)*((X-1)**2)*(1+2*X))
+        denom = X**3
+        d2Xdr2 = num/denom
+        return d2Xdr2
+
+    def get_x_from_r(self,r):
+        x = np.sqrt(r**2 - self.r_h**2)
+        return x
+
+    def get_r_from_x(self,x):
+        r = np.sqrt(x**2 + self.r_h**2)
+        return r
--- test_domain.py
+++ test_domain.py
@@ -2,7 +2,7 @@
 
 """test_domain.py
 Author: Jonah Miller (jonah.maxwell.miller@gmail.com)
-Time-stamp: <2017-05-12 08:41:08 (jmiller)>
+Time-stamp: <2017-05-12 11:07:12 (jmiller)>
 
 Tests the domain module of pyballd.
 """
@@ -10,6 +10,8 @@ Tests the domain module of pyballd.
 from __future__ import print_function
 import numpy as np
 from domain import PyballdStencil
+import matplotlib as mpl
+from matplotlib import pyplot as plt
 
 r_h = 1.
 THETA_ORDER = 20
@@ -18,13 +20,65 @@ THETA_MAX = np.pi/2
 f = lambda r,theta: np.sin(theta)/r
 dfdr = lambda r,theta: -np.sin(theta)/(r*r)
 
-def test_pointwis_error():
+def test_func_and_derivative():
+    X_order = 24
+    s = PyballdStencil(X_order,r_h,
+                       THETA_ORDER,
+                       THETA_MIN,THETA_MAX)
+    R,THETA = s.get_coords_2d()
+    X,THETA = s.get_x2d()
+    f_ana = f(R,THETA)
+    dfdr_ana = dfdr(R,THETA)
+    dfdr_ana[-1] = 0
+    dfdr_num = s.differentiate_wrt_R(f_ana,1,0)
+    dfdr_num[-1] = 0
+    plt.plot(X[:,-1],s.dXdR[:,-1],lw=3)
+    plt.xlabel(r'$\sqrt{r^2-r_h^2}/(1+\sqrt{r^2-r_h^2})$',fontsize=16)
+    plt.ylabel(r'$\partial_r X$',fontsize=16)
+    for postfix in ['.png','.pdf']:
+        plt.savefig('figs/domain_dXdr'+postfix,
+                    bbox_inches='tight')
+    plt.clf()
+
+    plt.plot(X[:,-1],f_ana[:,-1],'b-',lw=3)
+    plt.xlabel(r'$\sqrt{r^2-r_h^2}/(1+\sqrt{r^2-r_h^2})$',fontsize=16)
+    plt.ylabel(r'$\sin(\theta)/r$',fontsize=16)
+    for postfix in ['.png','.pdf']:
+        plt.savefig('figs/domain_test_function'+postfix,
+                    bbox_inches='tight')
+    plt.clf()
+
+    plt.plot(X[:,-1],dfdr_ana[:,-1],'b-',lw=3,label='analytic')
+    plt.plot(X[:,-1],dfdr_num[:,-1],'bo',lw=3,label='numerical')
+    plt.xlabel(r'$\sqrt{r^2-r_h^2}/(1+\sqrt{r^2-r_h^2})$',fontsize=16)
+    plt.ylabel(r'$\partial_r \sin(\theta)/r$',fontsize=16)
+    for postfix in ['.png','.pdf']:
+        plt.savefig('figs/deriv_domain_test_function'+postfix,
+                    bbox_inches='tight')
+    plt.clf()
+
+def test_pointwise_errors():
     X_orders = [8*(i+1) for i in range(3)]
-    for o in orders:
+    for o in X_orders:
         s = PyballdStencil(o,r_h,
-                           theta_order,
+                           THETA_ORDER,
                            THETA_MIN,THETA_MAX)
         R,THETA = s.get_coords_2d()
+        X,THETA = s.get_x2d()
         f_ana = f(R,THETA)
-        
-    
+        dfdr_ana = dfdr(R,THETA)
+        dfdr_ana[-1] = 0
+        dfdr_num = s.differentiate_wrt_R(f_ana,1,0)
+        dfdr_num[-1] = 0
+        delta = dfdr_num - dfdr_ana
+        plt.plot(X[:,-1],delta[:,-1],lw=3)
+    plt.xlabel(r'$\sqrt{r^2-r_h^2}/(1+\sqrt{r^2-r_h^2})$',fontsize=16)
+    plt.ylabel('error',fontsize=16)
+    for postfix in ['.png','.pdf']:
+        plt.savefig('figs/domain_pointwise_errors'+postfix,
+                    bbox_inches='tight')
+    plt.clf()
+
+if __name__ == "__main__":
+    test_func_and_derivative()
+    test_pointwise_errors()
