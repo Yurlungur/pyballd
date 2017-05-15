5cb796e689b5b3dfa78a78b733b21a45dea48c69
Jonah Miller <jonah.maxwell.miller@gmail.com>
2017-05-15 19:37:48 -0400

    added interpolation tests to test_orthopoly

--- test/test_orthopoly.py
+++ test/test_orthopoly.py
@@ -3,7 +3,7 @@
 """
 test_orthopoly.py
 Author: Jonah Miller (jonah.maxwell.miller@gmail.com)
-Time-stamp: <2017-05-13 16:24:23 (jmiller)>
+Time-stamp: <2017-05-15 19:35:25 (jmiller)>
 
 Tests the orthopoly module.
 """
@@ -71,9 +71,58 @@ def test_derivatives():
         plt.savefig('figs/orthopoly_errors'+postfix,
                     bbox_inches='tight')
     plt.clf()
-    
+
+def test_interp_at_order(ordery):
+    orderx = X_OVER_Y*ordery
+    s = PseudoSpectralStencil2D(orderx,XMIN,XMAX,
+                                ordery,YMIN,YMAX)
+    Xc,Yc = s.get_x2d()
+    x = np.linspace(XMIN,XMAX,100)
+    y = np.linspace(YMIN,YMAX,100)
+    Xf,Yf = np.meshgrid(x,y,indexing='ij')
+    f_coarse = f(Xc,Yc)
+    f_fine = f(Xf,Yf)
+    f_interpolator = s.to_continuum(f_coarse)
+    f_num = f_interpolator(Xf,Yf)
+    delta = f_num - f_fine
+    return np.max(np.abs(delta))
+
+def plot_interpolation(orderx,ordery):
+    s = PseudoSpectralStencil2D(orderx,XMIN,XMAX,
+                                ordery,YMIN,YMAX)
+    Xc,Yc = s.get_x2d()
+    x = np.linspace(XMIN,XMAX,100)
+    y = np.linspace(YMIN,YMAX,100)
+    Xf,Yf = np.meshgrid(x,y,indexing='ij')
+    f_coarse = f(Xc,Yc)
+    f_interpolator = s.to_continuum(f_coarse)
+    f_num = f_interpolator(Xf,Yf)
+    plt.pcolor(Xf,Yf,f_num)
+    cb = plt.colorbar()
+    cb.set_label('interpolated function',fontsize=16)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    for postfix in ['.png','.pdf']:
+        plt.savefig('figs/orthopoly_interpolated_function'+postfix,
+                    bbox_inches='tight')
+    plt.clf()
+
+def test_interpolation():
+    xfine = np.linspace(XMIN,XMAX,100)
+    yfine = np.linspace(YMIN,YMAX,100)
+    orders = [4+(2*i) for i in range(12)]
+    errors = [test_interp_at_order(o) for o in orders]
+    plt.semilogy(orders,errors,'bo-',lw=2,ms=12)
+    plt.xlabel('order in y-direction',fontsize=16)
+    plt.ylabel('max(interpolation error)',fontsize=16)
+    for postfix in ['.png','.pdf']:
+        plt.savefig('figs/orthopoly_interp_errors'+postfix,
+                    bbox_inches='tight')
+    plt.clf()
 
 if __name__ == "__main__":
     plot_test_function(80,160)
     test_derivatives()
+    plot_interpolation(10,20)
+    test_interpolation()
 
